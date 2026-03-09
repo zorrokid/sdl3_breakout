0793cc7ab6508939afe5abfcaf0d0069a777394a
Mikko Keinänen <mikko.keinanen@gmail.com>
2026-03-09 23:56:40 +0200

    feat: move paddle

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_keycode.h>
 #include <SDL3/SDL_render.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
@@ -9,6 +12,8 @@ typedef struct {
     SDL_Renderer *renderer;
     bool running;
     SDL_FRect paddle;
+    bool left_pressed;
+    bool right_pressed;
 } GameContext;
 
 // 2. Setup: This runs exactly once when the program starts
@@ -43,15 +48,47 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
 // 3. Events: Handle "X" button or keys
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    GameContext *ctx = (GameContext *)appstate;
+
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS; // This exits the app cleanly
     }
+
+    if (event->type == SDL_EVENT_KEY_DOWN || event->type == SDL_EVENT_KEY_UP) {
+        bool is_down = (event->type == SDL_EVENT_KEY_DOWN);
+
+        switch (event->key.key) {
+        case SDLK_LEFT:
+            ctx->left_pressed = is_down;
+            break;
+        case SDLK_RIGHT:
+            ctx->right_pressed = is_down;
+            break;
+        case SDLK_ESCAPE:
+            return SDL_APP_SUCCESS;
+            break;
+        }
+    }
+
     return SDL_APP_CONTINUE;
 }
 
 // 4. The Loop: This runs every single frame
 SDL_AppResult SDL_AppIterate(void *appstate) {
     GameContext *ctx = (GameContext *)appstate;
+
+    // Move the paddle
+    float paddle_speed = 10.0f; // TODO: add delta time
+    if (ctx->left_pressed)
+        ctx->paddle.x -= paddle_speed;
+    if (ctx->right_pressed)
+        ctx->paddle.x += paddle_speed;
+
+    // Check screen boundaries
+    if (ctx->paddle.x < 0)
+        ctx->paddle.x = 0;
+    if (ctx->paddle.x > 800 - ctx->paddle.w)
+        ctx->paddle.x = 800 - ctx->paddle.w;
 
     // A. Clear the screen (Black)
     SDL_SetRenderDrawColor(ctx->renderer, 0, 0, 0, 255);
